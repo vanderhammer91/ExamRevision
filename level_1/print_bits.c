19fb2cd1a58e5d9e4775b7f7fce5aef2ea4cefd2
vanderhammer91 <120767153+vanderhammer91@users.noreply.github.com>
2023-04-01 21:46:46 +1030

    Add files via upload

---
+++ level_1/print_bits.c
@@ -0,0 +1,40 @@
+/*
+Assignment name  : print_bits
+Expected files   : print_bits.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a function that takes a byte, and prints it in binary WITHOUT A NEWLINE
+AT THE END.
+
+Your function must be declared as follows:
+
+void	print_bits(unsigned char octet);
+
+Example, if you pass 2 to print_bits, it will print "00000010"%
+*/
+#include <unistd.h>
+
+void	print_bits(unsigned char octet)
+{
+	int				i;
+	unsigned char	bit;
+	char			c;
+
+	i = 7;
+	while(i >= 0)
+	{
+		bit = (octet >> i) & 1;
+		c = bit + '0';
+		write(1, &c, 1);
+		i--;
+	}
+}
+
+/*
+int	main(void)
+{
+	print_bits(10);
+	return (0);
+}
+*/
