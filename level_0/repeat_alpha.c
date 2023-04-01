0f5c32feb31243df16b8e80a62734d690814aee0
vanderhammer91 <120767153+vanderhammer91@users.noreply.github.com>
2023-04-01 23:24:23 +1030

    Update repeat_alpha.c

--- level_0/repeat_alpha.c
+++ level_0/repeat_alpha.c
@@ -51,13 +51,6 @@ int is_upper(char c)
 	return (0);
 }
 
-int is_alpha(char c)
-{
-	if 	((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	return (0);
-}
-
 int	get_index(char c)
 {
 	if(is_upper(c))
