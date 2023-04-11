4b31ebf028f7a08b950f236bbfadba345222360d
Isaac Vanderwal <120767153+vanderhammer91@users.noreply.github.com>
2023-04-11 21:30:22 +0930

    A more concise implementation

--- level_2/epur_str.c
+++ level_2/epur_str.c
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   epur_str.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ivanderw <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/25 16:53:18 by ivanderw          #+#    #+#             */
-/*   Updated: 2023/03/25 18:30:41 by ivanderw         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 /*
 Assignment name  : epur_str
 Expected files   : epur_str.c
@@ -36,118 +25,59 @@ $
 $> ./epur_str "" | cat -e
 $
 $>
-
-$> ./epur_str "vous voyez c'est facile d'afficher la meme chose" | cat -e
-vous voyez c'est facile d'afficher la meme chose$
-$> ./epur_str " seulement          la c'est      plus dur " | cat -e
-seulement la c'est plus dur$
-$> ./epur_str "comme c'est cocasse" "vous avez entendu, Mathilde ?" | cat -e
-$
-$> ./epur_str "" | cat -e
-$
-$>
 */
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-int	is_delim(char c)
+int	is_space(char c)
 {
-	if (c == ' ' || c == '\t' || c == '\n' || c == '\r')
+	if (c == ' ' || c == '\t')
 		return (1);
 	return (0);
 }
 
-size_t get_epur_length(char *str)
+int	end_check(char *str)
 {
-	size_t	count;
-	int	prev_is_space;
-
-	prev_is_space = 0;
-	count = 0;
-	while (*str && is_delim(*str))
+	while(*str)
+	{
+		if (!is_space(*str))
+				return (0);
 		str++;
-	while (*str)
-	{
-		if (is_delim(*str) && !prev_is_space)
-		{
-			prev_is_space = 1;
-			str++;	
-			count++;
-		}
-		else if(is_delim(*str) && prev_is_space)
-		{
-			str++;
-		}
-		else
-		{
-			prev_is_space = 0;
-			count++;
-			str++;
-		}
 	}
-	str--;
-	while (is_delim(*str))
-	{
-		str--;
-		count--;
-	}			
-	return (count);
+	return (1);
 }
-
 
 void	epur_str(char *str)
 {
-	size_t 	epur_len;
-	size_t	i;
-	int	prev_is_space;
+	int i = 0;
 
-	epur_len = get_epur_length(str);
-	if (!epur_len)
+	while (str[i])
 	{
-		ft_putchar('\n');
-		return ;
-	}
-	while (*str && is_delim(*str))
-		str++;
-
-	i = 0;
-	prev_is_space = 0;
-	while(*str && i < epur_len)
-	{
-		if (is_delim(*str) && !prev_is_space)
+		if (is_space(str[i]))
+				i++;
+		if (!is_space(str[i]))
 		{
-			prev_is_space = 1;
-			ft_putchar(*str);
-			i++;
-			str++;	
+			while (str[i] && !is_space(str[i]))
+			{
+				ft_putchar(str[i]);
+				i++;
+			}
+			if (!end_check(&str[i]))
+			{
+				ft_putchar(' ');
+			}
 		}
-		else if(is_delim(*str) && prev_is_space)
-		{
-			str++;
-		}
-		else if(!is_delim(*str))
-		{
-			prev_is_space = 0;
-			ft_putchar(*str);	
-			i++;
-			str++;
-		}		
 	}
-	ft_putchar('\n');
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc == 2)
 		epur_str(argv[1]);
-	else
-		ft_putchar('\n');	
+	ft_putchar('\n');
 	return (0);
 }
