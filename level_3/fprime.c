5702fe97bde75038e5a26a214067d610ba1fff0a
Isaac Vanderwal <120767153+vanderhammer91@users.noreply.github.com>
2023-04-20 12:42:45 +0930

    Shorter, uses printf and atoi (as we are permitted)

--- level_3/fprime.c
+++ level_3/fprime.c
@@ -2,19 +2,13 @@
 Expected files   : fprime.c
 Allowed functions: printf, atoi
 --------------------------------------------------------------------------------
-
 Write a program that takes a positive int and displays its prime factors on the
 standard output, followed by a newline.
-
 Factors must be displayed in ascending order and separated by '*', so that
 the expression in the output gives the right result.
-
 If the number of parameters is not 1, simply display a newline.
-
 The input, when there is one, will be valid.
-
 Examples:
-
 $> ./fprime 225225 | cat -e
 3*3*5*5*7*11*13$
 $> ./fprime 8333325 | cat -e
@@ -31,38 +25,15 @@ $> ./fprime | cat -e
 $
 $> ./fprime 42 21 | cat -e
 $	*/
-#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-void	ft_putchar(char c)
+int	is_prime(int n)
 {
-	write (1, &c, 1);
-}
-
-int	ft_atoi(char *str)
-{
-	int result = 0;
-	int sign = 1;
-
-	while (*str == ' ' || *str == '\t')
-		str++;
-	if(*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign = - sign;
-		str++;
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		result = (result * 10) + (*str - '0');
-		str++;
-	}
-	return (result * sign);
-}
-
-int ft_isprime(int n)
-{
+	if (n < 2)
+		return (0);
 	int i = 2;
-	while (i <= (n / 2))
+	while (i <= n / 2)
 	{
 		if (n % i == 0)
 			return (0);
@@ -71,60 +42,30 @@ int ft_isprime(int n)
 	return (1);
 }
 
-void	ft_putnbr(int n)
-{
-	if (n < 0)
-	{
-		ft_putchar('-');
-		ft_putnbr(-n);
-	}
-	else if (n < 10)
-	{
-		ft_putchar(n + '0');
-	}
-	else if (n >= 10)
-	{
-		ft_putnbr(n / 10);
-		ft_putchar((n % 10) + '0');
-	}
-}
-
 void	fprime(int n)
 {
-
-	if(n == 1)
-	{
-		ft_putchar('0');
-		return ;
-	}
-	else if (n == 2)
-	{
-		ft_putchar('1');
-		return ;
-	}
 	int i = 2;
-	while (i <= n / 2)
+	while (i <= n/2)
 	{
-		if (ft_isprime(i) && n % i == 0)
+		while (is_prime(i) && n % i == 0)
 		{
+			printf("%d*", i);
 			n = n / i;
-			ft_putnbr(i);
-			ft_putchar('*');
 		}
-		else
-			i++;
+		i++;
 	}
-	ft_putnbr(n);
+	printf("%d",n);
 }
 
-int	main(int argc, char **argv)
+int	main(int argc,  char **argv)
 {
 	if (argc == 2)
 	{
-		int result = ft_atoi(argv[1]);
+		int result = atoi(argv[1]);
 		if (result > 0)
 			fprime(result);
 	}
-	ft_putchar('\n');
+
+	printf("\n");
 	return (0);
-}
+}	
